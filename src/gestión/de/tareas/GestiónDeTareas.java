d1bdeb67a6b81cd11597df8e691d14fa3e295f24
Diego Govea <87109033+diegogovea@users.noreply.github.com>
2024-03-16 12:59:37 -0600

    Create GestiónDeTareas.java

---
+++ src/gestión/de/tareas/GestiónDeTareas.java
@@ -0,0 +1,11 @@
+
+package gestión.de.tareas;
+
+public class GestiónDeTareas {
+    
+    public static void main(String[] args) {
+        usuario nf=new usuario();
+        nf.setVisible(true);
+    }
+    
+}
